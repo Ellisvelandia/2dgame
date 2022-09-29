20b57c6b89b4ccb00f0b001ca52f1bf3da0bfa37
ellisvelandia <eyis619@gmail.com>
2022-09-29 15:20:35 -0500

    2d

--- script.js
+++ script.js
@@ -8,18 +8,20 @@ window.addEventListener("load", function () {
     constructor(game) {
       this.game = game;
       window.addEventListener("keydown", (e) => {
-        if (e.key === "ArrowUp") {
+        if (
+          (e.key === "ArrowUp" || e.key === "ArrowDown") &&
+          this.game.keys.indexOf(e.key) === -1
+        ) {
           this.game.keys.push(e.key);
         }
         console.log(this.game.keys);
       });
-      window -
-        addEventListener("keyup", (e) => {
-          if (this.game.keys.indexOf(e.key) > -1) {
-            this.game.keys.splice(this, game.keys.indexOf(e.key), 1);
-          }
-          console.log(this.game.keys);
-        });
+      window.addEventListener("keyup", (e) => {
+        if (this.game.keys.indexOf(e.key) > -1) {
+          this.game.keys.splice(this, game.keys.indexOf(e.key), 1);
+        }
+        console.log(this.game.keys);
+      });
     }
   }
 
@@ -37,6 +39,8 @@ window.addEventListener("load", function () {
       this.speedY = 0;
     }
     update() {
+      if (this.game.keys.includes("ArrowUp")) this.speedY = -1;
+      else if (this.game.keys.includes("ArrowDown")) this.speedY = 1;
       this.y += this.speedY;
     }
     draw(context) {
