3b1c0694a6a25592fbaab38b1dda98c72b4d9950
ellisvelandia <eyis619@gmail.com>
2022-09-29 20:20:03 -0500

    2dgame

--- script.js
+++ script.js
@@ -15,6 +15,8 @@ window.addEventListener("load", function () {
           this.game.keys.push(e.key);
         } else if (e.key === " ") {
           this.game.player.shootTop();
+        } else if (e.key === "d") {
+          this.game.debug = !this.game.debug;
         }
       });
       window.addEventListener("keyup", (e) => {
@@ -54,10 +56,15 @@ window.addEventListener("load", function () {
       this.height = 190;
       this.x = 20;
       this.y = 100;
+      this.frameX = 0;
+      this.frameY = 0;
+      this.maxFrame = 37;
       this.speedY = 3;
       this.maxSpeed = 5;
       this.projectiles = [];
+      this.image = document.getElementById("player");
     }
+
     update() {
       if (this.game.keys.includes("ArrowUp")) this.speedY = -this.maxSpeed;
       else if (this.game.keys.includes("ArrowDown"))
@@ -71,10 +78,27 @@ window.addEventListener("load", function () {
       this.projectiles = this.projectiles.filter(
         (projectile) => !projectile.markedForDeletion
       );
+      // sprite animation
+      if (this.frameX < this.maxFrame) {
+        this.frameX++;
+      } else {
+        this.frameX = 0;
+      }
     }
     draw(context) {
-      context.fillStyle = "black";
-      context.fillRect(this.x, this.y, this.width, this.height);
+      if (this.game.debug)
+        context.strokeRect(this.x, this.y, this.width, this.height);
+      context.drawImage(
+        this.image,
+        this.frameX * this.width,
+        this.frameY * this.height,
+        this.width,
+        this.height,
+        this.x,
+        this.y,
+        this.width,
+        this.height
+      );
       this.projectiles.forEach((projectile) => {
         projectile.draw(context);
       });
@@ -237,7 +261,9 @@ window.addEventListener("load", function () {
       this.gameTime = 0;
       this.timeLimit = 5000;
       this.speed = 1;
+      this.debug = true;
     }
+
     update(deltaTime) {
       if (!this.gameOver) this.gameTime += deltaTime;
       if (this.gameTime > this.timeLimit) this.gameOver = true;
