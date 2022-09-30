2a18167895bfe453712121edd047b743b5b16055
ellisvelandia <eyis619@gmail.com>
2022-09-30 16:46:12 -0500

    2dgame

--- script.js
+++ script.js
@@ -350,6 +350,57 @@ window.addEventListener("load", function () {
     }
   }
 
+  class Explosion {
+    constructor(game, x, y) {
+      this.game = game;
+      this.frameX = 0;
+      this.spriteHeight = 200;
+      this.fps = 15;
+      this.timer = 0;
+      this.interval = 1000 / this.fps;
+      this.markedForDeletion = false;
+      this.maxFrame = 8;
+    }
+    update(deltaTime) {
+      if (this.timer > this.interval) {
+        this.frameX++;
+        this.timer= 0;
+      } else {
+        this.timer += deltaTime;
+      }
+      if (this.frameX > this.maxFrame) this.markedForDeletion = true;
+    }
+    draw(context) {
+      context.drawImage(
+        this.image,
+        this.frameX * this.spriteWidth,
+        0,
+        this.spriteWidth,
+        this.spriteHeight,
+        this.x,
+        this.y,
+        this.width,
+        this.height
+      );
+    }
+  }
+
+  class SmokeExplosion extends Explosion {
+    constructor(game, x, y) {
+      super(game, x, y);
+      this.image = document.getElementById("smokeExplosion");
+      this.spriteWidth = 200;
+      this.width = this.spriteWidth;
+      this.height = this.spriteHeight;
+      this.x = x - this.width * 0.5;
+      this.y = y - this.height * 0.5;
+    }
+  }
+
+  class FireExplosion extends Explosion {
+    constructor(game, x, y) {}
+  }
+
   class UI {
     constructor(game) {
       this.game = game;
@@ -414,6 +465,7 @@ window.addEventListener("load", function () {
       this.keys = [];
       this.enemies = [];
       this.particles = [];
+      this.explosions = [];
       this.enemyTimer = 0;
       this.enemyInterval = 1000;
       this.ammo = 20;
@@ -445,10 +497,15 @@ window.addEventListener("load", function () {
       this.particles = this.particles.filter(
         (particle) => !particle.markedForDeletion
       );
+      this.explosions.forEach((explosion) => explosion.update(deltaTime));
+      this.explosions = this.explosions.filter(
+        (explosion) => !explosion.markedForDeletion
+      );
       this.enemies.forEach((enemy) => {
         enemy.update();
         if (this.checkCollision(this.player, enemy)) {
           enemy.markedForDeletion = true;
+          this.addExplosion(enemy);
           for (let i = 0; i < enemy.score; i++) {
             this.particles.push(
               new Particle(
@@ -474,6 +531,7 @@ window.addEventListener("load", function () {
             );
             if (enemy.lives <= 0) {
               enemy.markedForDeletion = true;
+              this.addExplosion(enemy);
               if (enemy.type === "hive") {
                 for (let i = 0; i < 5; i++) {
                   this.enemies.push(
@@ -507,14 +565,26 @@ window.addEventListener("load", function () {
       this.enemies.forEach((enemy) => {
         enemy.draw(context);
       });
+
+      this.explosions.forEach((explosion) => {
+        explosion.draw(context);
+      });
+
       this.background.layer4.draw(context);
     }
     addEnemy() {
       const randomize = Math.random();
-      if (randomize < 0.3) this.enemies.push(new Angler1(this));
+      if (randomize < 0.3) this.explosions.push(new Angler1(this));
       else if (randomize < 0.6) this.enemies.push(new Angler2(this));
       else if (randomize < 0.8) this.enemies.push(new HiveWhale(this));
       else this.enemies.push(new LuckyFish(this));
+    }
+
+    addExplosion(enemy) {
+      const randomize = Math.random();
+      if (randomize < 1)
+        this.explosions.push(new SmokeExplosion(this, enemy.x, enemy.y));
+      console.log(this.explosions);
     }
     checkCollision(rect1, rect2) {
       return (
